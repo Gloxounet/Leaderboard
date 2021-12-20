27c102541dbe62d301f992c3064cdd6997bec981
Matteo Glock <matteo.glock@laposte.net>
2021-12-20 17:55:43 +0100

    sql implementation

---
+++ sql_requests.py
@@ -0,0 +1,109 @@
+import mysql.connector
+from mysql.connector import Error
+import sqlparse
+
+#Request sender
+def getCustomRequest(sql_request:str,pretify=False):
+    try:
+        connection = mysql.connector.connect(host='localhost',
+                                            database='data_leaderboard',
+                                            user='root',
+                                            password='root')
+        if connection.is_connected():
+            db_Info = connection.get_server_info()
+            print("Connected to MySQL Server version ", db_Info)
+            cursor = connection.cursor()
+            if pretify :
+                print("\n#########SENDING REQUEST#############")
+                print(sqlparse.format(sql_request, reindent=True, keyword_case='upper'))
+                print("######################################\n")
+            cursor.execute(sql_request)
+            records = cursor.fetchall()
+
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+            return records
+        
+def sendCustomRequest(sql_request:str,pretify=False):
+    try:
+        connection = mysql.connector.connect(host='localhost',
+                                            database='data_leaderboard',
+                                            user='root',
+                                            password='root')
+        if connection.is_connected():
+            db_Info = connection.get_server_info()
+            print("Connected to MySQL Server version ", db_Info)
+            cursor = connection.cursor()
+            if pretify :
+                print("\n#########INSERTING RECORD#############")
+                print(sqlparse.format(sql_request, reindent=True, keyword_case='upper'))
+                print("######################################\n")
+            cursor.execute(sql_request)
+            connection.commit()
+            print(cursor.rowcount, "record inserted.")
+
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+def deleteCustomRequest(sql_request:str,pretify=False):
+    try:
+        connection = mysql.connector.connect(host='localhost',
+                                            database='data_leaderboard',
+                                            user='root',
+                                            password='root')
+        if connection.is_connected():
+            db_Info = connection.get_server_info()
+            print("Connected to MySQL Server version ", db_Info)
+            cursor = connection.cursor()
+            if pretify :
+                print("#########DELETING RECORDS#############")
+                print(sqlparse.format(sql_request, reindent=True, keyword_case='upper'))
+                print("######################################")
+            cursor.execute(sql_request)
+            connection.commit()
+            print("deleted.")
+
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+#Request creator
+def createCustomRequest(select:str="*", table:str="défis", where:str=None)->str:
+    q = f"""SELECT {select} FROM {table}"""
+    if where != None :
+        q = q + f""" WHERE {where}"""
+    return q
+
+def createCustomInsert(table:str="défis",column_list:list[str]=[],values_list:list=[]):
+    #Formatage colonnes
+    colonnes = column_list.__str__()[1:-1]
+    colonnes = [i for i in colonnes if i!="\'"]
+    colonnes = "(" + "".join(colonnes) + ")"
+    values = "(" + values_list.__str__()[1:-1] + ")"
+    
+    return f"""INSERT INTO {table}""" + colonnes + f"""VALUES {values}"""
+
+def createCustomDelete(table:str="défis",where:str="id = 1") :
+    q = f"""DELETE FROM {table} WHERE {where}"""
+    return q
+    
+
+if __name__ == "__main__":
+    print(getCustomRequest("""SELECT * from défis""",pretify=True))
+    i = createCustomInsert("défis",["id","name","coef","solo"],["1","Lancer de lances",0.3,True])
+    sendCustomRequest(i,pretify=True)
+    deleteCustomRequest(createCustomDelete(),pretify=True)
